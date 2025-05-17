666b934db5c5ce81595b5a955e31e4f0a5257dee
Chonghuan.Wang <ch.wang@ennovationtech.eu>
2025-05-17 22:22:18 +0100

    add shadcn sidebar 15

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button"
-
+import Page from "@/app/dashboard/page"
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <Page />
   )
 }
 
